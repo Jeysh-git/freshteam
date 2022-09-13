9045eaa8580026b82d79e5cf21a4d5ded277546d
Jeysh-git <101196204+Jeysh-git@users.noreply.github.com>
2022-09-13 19:33:37 +0530

    Add files via upload

---
+++ dist/assets/tests.js
@@ -0,0 +1,71 @@
+'use strict';
+
+define('freshteam/tests/lint/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+});
+define('freshteam/tests/lint/templates.template.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('TemplateLint');
+
+  QUnit.test('freshteam/templates/application.hbs', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'freshteam/templates/application.hbs should pass TemplateLint.\n\n');
+  });
+});
+define('freshteam/tests/lint/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+});
+define('freshteam/tests/test-helper', ['freshteam/app', 'freshteam/config/environment', '@ember/test-helpers', 'ember-qunit'], function (_app, _environment, _testHelpers, _emberQunit) {
+  'use strict';
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+
+  (0, _emberQunit.start)();
+});
+define('freshteam/config/environment', [], function() {
+  var prefix = 'freshteam';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+require('freshteam/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
